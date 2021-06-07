6500648d933b57c9655ae92c35da880b2c7d5620
DESKTOP-BIBNNHT\82105 <hanyk9656@gmail.com>
2021-06-07 15:17:33 +0900

    operator change

--- src/main/java/hello/hellospring/service/MemberService.java
+++ src/main/java/hello/hellospring/service/MemberService.java
@@ -2,14 +2,12 @@ package hello.hellospring.service;
 
 import hello.hellospring.domain.Member;
 import hello.hellospring.repository.MemberRepository;
-import hello.hellospring.repository.MemoryMemberRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Optional;
 
-
+@Transactional
 public class MemberService {
     private final MemberRepository memberRepository;
 
@@ -20,29 +18,29 @@ public class MemberService {
     /**
      * 회원가입
      */
-    public Long join(Member member){
-        //같은 이름이 있는 중복 회원 x
-        validateDuplicateMember(member);
-        memberRepository.save(member);
-        return member.getId();
+    public Long join(Member member) {
 
+            //같은 이름이 있는 중복 회원 x
+            validateDuplicateMember(member);
+            memberRepository.save(member);
+        return member.getId();
     }
 
     private void validateDuplicateMember(Member member) {
         memberRepository.findByName(member.getName())
-                    .ifPresent(m -> {
-                        throw new IllegalStateException("이미 존재하는 회원입니다.");
-                    });
+                .ifPresent(m -> {
+                    throw new IllegalStateException("이미 존재하는 회원입니다.");
+                });
     }
 
     /**
      * 전체 회원 조회
      */
-    public List<Member> findMembers(){
-        return  memberRepository.findAll();
+    public List<Member> findMembers() {
+            return memberRepository.findAll();
     }
 
-    public Optional <Member> findOne(Long memberId) {
+    public Optional<Member> findOne(Long memberId) {
         return memberRepository.findById(memberId);
     }
 
